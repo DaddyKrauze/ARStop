ea910131e874b948cef372724958cd7a851fa605
DaddyKrauze <anton53429@gmail.com>
2020-07-02 15:53:18 +0300

    Initial Commit

---
+++ ARStop/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ARStop
+//
+//  Created by Tony Krauze on 02.07.2020.
+//  Copyright © 2020 Tony Krauze. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
